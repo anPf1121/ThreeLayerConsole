0d7932e53d9ec189526738e5997b38bb788bfb79
Pain281 <an.nguyenthien281@gmail.com>
2023-08-12 18:49:15 +0700

    set up test va them huong dan su dung thu vien xunit de test

--- PhoneStorePL.Tests/UnitTest1.cs
+++ PhoneStorePL.Tests/UnitTest1.cs
@@ -1,11 +1,18 @@
 using Ults;
+using Xunit;
 
 namespace PhoneStorePL.Tests;
 
-public class UnitTest1
+// cách chạy test dotnet test    
+public class OrderTests
 {
-    [Fact]
-    public void Test1()
+    [Fact] //[Fact] là một thuộc tính (attribute) được sử dụng để đánh dấu một phương thức là một test method. Khi bạn đánh dấu một phương thức bằng [Fact], xUnit sẽ biết rằng phương thức đó cần được chạy như một test và sẽ thực hiện kiểm tra kết quả của nó.
+    public void CreateValidOrder_Success()
     {
+        int a = 5;
+        int b = 7;
+        int result = 12;
+        Assert.Equal(12, result);
     }
+
 }
